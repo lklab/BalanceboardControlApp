740e577ed933b5927e5504c80cbab5fba6e4eb11
khlee <alexeibatic@gmail.com>
2019-07-31 11:47:38 +0900

    change layout

--- app/src/main/java/com/gambridzy/balanceboardcontrolapp/MainActivity.kt
+++ app/src/main/java/com/gambridzy/balanceboardcontrolapp/MainActivity.kt
@@ -2,12 +2,23 @@ package com.gambridzy.balanceboardcontrolapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity()
 {
+    private var synchronizedStatusText: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        synchronizedStatusText = findViewById(R.id.synchronizedStatusText)
+    }
+
+    fun onSynchronizedStartButtonClick(view: View)
+    {
+        synchronizedStatusText?.text = "hello"
     }
 }
